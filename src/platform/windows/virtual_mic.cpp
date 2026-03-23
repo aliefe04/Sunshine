8c85f3156e6699929bebd19da949db8f7798c232
ALI EFE CAKICI <efecakici98@gmail.com>
2026-03-23 22:36:27 +0300

    fix: use DEFINE_PROPERTYKEY instead of header for PKEY_Device_FriendlyName

--- src/platform/windows/virtual_mic.cpp
+++ src/platform/windows/virtual_mic.cpp
@@ -9,12 +9,13 @@
 // platform includes
 #include <Audioclient.h>
 #include <mmdeviceapi.h>
-#include <functiondiscoverykeys_devpkey.h>
 
 // local includes
 #include "src/config.h"
 #include "src/logging.h"
 #include "virtual_mic.h"
+
+DEFINE_PROPERTYKEY(PKEY_Device_FriendlyName, 0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0, 14);  // DEVPROP_TYPE_STRING
 
 namespace platf::virtual_mic {
   using namespace std::literals;
