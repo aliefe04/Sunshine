58a1632a466f403d081c948686ee93b566a321da
ALI EFE CAKICI <efecakici98@gmail.com>
2026-03-23 22:55:36 +0300

    fix: add missing utility.h include for util::hex

--- src/platform/windows/virtual_mic.cpp
+++ src/platform/windows/virtual_mic.cpp
@@ -13,6 +13,7 @@
 // local includes
 #include "src/config.h"
 #include "src/logging.h"
+#include "src/utility.h"
 #include "virtual_mic.h"
 
 DEFINE_PROPERTYKEY(PKEY_Device_FriendlyName, 0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0, 14);  // DEVPROP_TYPE_STRING
